5df51c312167505e803242fa95fca6163a87f1ba
Luiz Costa <gutomcosta@gmail.com>
2015-02-25 23:20:25 -0300

    remove outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,7 +14,21 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    residuals = predictions - net_worths
+    for i in range(0, len(residuals)):
+        tuple = (ages[i][0], net_worths[i][0], residuals[i][0])
+        cleaned_data.append(tuple)
 
+    remove_count = int(round(len(cleaned_data) - (len(cleaned_data) * 0.9)))   
+    ordered_list = sorted(cleaned_data, key=lambda t: t[2], reverse=True)
+
+    print "clean before: ", len(cleaned_data)
+
+    for i in range(0, remove_count):
+        tuple = ordered_list[i]
+        cleaned_data.remove(tuple)
+
+    print "clean after: ", len(cleaned_data)
     
     return cleaned_data
 
