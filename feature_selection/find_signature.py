9b2dcb7352ae2f599ac125f27b465587a6ba1e3c
Luiz Costa <gutomcosta@gmail.com>
2015-03-17 23:20:25 -0300

    feature selection

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -5,19 +5,20 @@ import numpy
 numpy.random.seed(42)
 
 
+print "Loading data"
 ### the words (features) and authors (labels), already largely processed
-words_file = "word_data_overfit.pkl" ### like the file you made in the last mini-project 
-authors_file = "email_authors_overfit.pkl"  ### this too
+words_file = "../text_learning/your_word_data.pkl" ### like the file you made in the last mini-project 
+authors_file =  "../text_learning/your_email_authors.pkl"  ### this too
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
 
-
+print "load finished"
 
 ### test_size is the percentage of events assigned to the test set (remainder go into training)
 from sklearn import cross_validation
 features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
-
+print "vectorizing"
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                              stop_words='english')
@@ -30,10 +31,36 @@ features_test  = vectorizer.transform(features_test).toarray()
 ### train on only 150 events to put ourselves in this regime
 features_train = features_train[:150]
 labels_train   = labels_train[:150]
-
+print " starting training decision tree"
 
 
 ### your code goes here
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+
+print "predicting"
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+print "accuracy is: ", acc
+
+important_features = clf.feature_importances_
+count = 0
+for feature in important_features:
+	if feature > 0.2:
+		print feature
+		print count
+	count += 1
+
+print "getting the feature name"
+feature_names = vectorizer.get_feature_names()
+print feature_names[33604]
+
+
+
 
 
 
